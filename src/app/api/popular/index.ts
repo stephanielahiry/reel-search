f7ca710322d9ac49930291fc3eda12390b797d30
Stephanie Lahiry <stephanielahiry@SHASHANKs-Air.lan>
2024-09-17 09:15:20 -0700

    added detailed error logging

--- src/app/api/popular/index.ts
+++ src/app/api/popular/index.ts
@@ -8,7 +8,13 @@ export async function GET() {
         const data = await apiFetch(endpoint);  
         return NextResponse.json(data);
     } catch (error) {
-        return NextResponse.json({ error: 'Failed to fetch search results' }, { status: 500 });
+        if (error instanceof Error) {
+            console.error('Error fetching data:', error);
+            return NextResponse.json({ error: 'Failed to fetch popular movies', details: error.message }, { status: 500 });
+        } else {
+            console.error('Unexpected error:', error);
+            return NextResponse.json({ error: 'Failed to fetch popular movies', details: 'An unexpected error occurred' }, { status: 500 });
+        }
     }
 }
   
