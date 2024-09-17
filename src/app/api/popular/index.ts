99f9362623c35b2df70f43e7cb13ea9f9ee19c40
Stephanie Lahiry <stephanielahiry@SHASHANKs-Air.lan>
2024-09-17 08:43:04 -0700

    changed api path

--- src/app/api/popular/index.ts
+++ src/app/api/popular/index.ts
@@ -3,7 +3,7 @@ import { BASE_URL } from '@/app/constants';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const endpoint = `${BASE_URL}/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}`; 
+    const endpoint = `https://api.themoviedb.org/3/movie/popular?api_key=4a578780b4555c6daabb01aa39a8f791`; 
     try {
         const data = await apiFetch(endpoint);  
         return NextResponse.json(data);
