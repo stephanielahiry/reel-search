7bf368709411d69031eb112258c4659a6c88ee22
Stephanie Lahiry <stephanielahiry@SHASHANKs-Air.lan>
2024-09-17 16:18:25 -0700

    added log

--- src/api/apiFetch.ts
+++ src/api/apiFetch.ts
@@ -5,6 +5,7 @@ export const apiFetch = async <returnType>(endpoint: string): Promise<returnType
       'Authorization': `API-Key ${process.env.NEXT_PUBLIC_API_KEY}`
     },
   });
+  console.log(response);
   if (!response.ok) throw new Error('Error fetching data!');
   const data = await response.json();
   return data;
