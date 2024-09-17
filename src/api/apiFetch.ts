c34366e224093b74a3589f55e1460fb02c00167d
Stephanie Lahiry <stephanielahiry@SHASHANKs-Air.lan>
2024-09-17 15:46:20 -0700

    updated headers

--- src/api/apiFetch.ts
+++ src/api/apiFetch.ts
@@ -2,7 +2,7 @@ export const apiFetch = async <returnType>(endpoint: string): Promise<returnType
   const response = await fetch(endpoint, {
     method: 'GET',
     headers: {
-      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
+      'Authorization': `API-Key ${process.env.NEXT_PUBLIC_API_KEY}`
     },
   });
   if (!response.ok) throw new Error('Error fetching data!');
