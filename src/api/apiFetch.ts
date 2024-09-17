93b10f38268912a761af9bd3abbced6eea06b156
Stephanie Lahiry <stephanielahiry@SHASHANKs-Air.lan>
2024-09-17 15:38:45 -0700

    updated fetch headers

--- src/api/apiFetch.ts
+++ src/api/apiFetch.ts
@@ -1,8 +1,11 @@
 export const apiFetch = async <returnType>(endpoint: string): Promise<returnType> => {
-    const request = new Request(endpoint, { method: 'GET'});
-    request.headers.append('Authorization', `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`);
-    const response = await fetch(request);
-    if (!response.ok) throw new Error('Error fetching data!');
-    const data = await response.json();
-    return data;
-  };
+  const response = await fetch(endpoint, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
+    },
+  });
+  if (!response.ok) throw new Error('Error fetching data!');
+  const data = await response.json();
+  return data;
+};
